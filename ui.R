7ccf5d610183ed6fa56113211fdca16ea4d0c8fd
nicolas-bofi <nicolas.bofi@etu.umontpellier.fr>
2021-12-16 16:01:03 +0100

    Start home page

--- ui.R
+++ ui.R
@@ -16,8 +16,8 @@ dashboardPage(
   ),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Accueil", tabName = "home", icon = icon("dashboard")),
-      menuItem("Graphiques", tabName = "graphics", icon = icon("th"))
+      menuItem("Accueil", tabName = "home", icon = icon("fas fa-home")),
+      menuItem("Graphiques", tabName = "graphics", icon = icon("far fa-chart-bar"))
     )
   ),
   dashboardBody(
@@ -30,14 +30,17 @@ dashboardPage(
       tabItem(tabName = "home",
         h1("Data Science Project - Balea"),
         h3("Analyse des balances potentiellement défectueuses des chariots utilisés dans un entrepôt de préparation de commande"),
-        h3("Problématique : Comment identifier de façon automatique les balances potentiellement défectueuses ?")
-        
+        h3("Problématique : Comment identifier de façon automatique les balances potentiellement défectueuses ?"),
+        box(
+          title = "Contexte", solidHeader = TRUE,
+          "Balea est une PME languedocienne qui fabrique et développe des chariots connectés de préparation de commandes pour les entrepôts logistiques. Ces chariots utilisent des balances ayant pour but de minimiser les erreurs humaines et favoriser l'exactitude des commandes préparées. Chaque chariot possède plusieurs balances connectées à un système embarqué qui permet d'enregistrer et de contrôler ces poids. Ce système de contrôle permet à Balea d'assurer une grande fiabilité dans la justesse des commandes préparées par les opérateurs. Dans la pratique, il arrive qu’une balance soit défectueuse. Cela engendre des erreurs de mesures de poids et par conséquent des interventions humaines sont nécessaires telles que le tarage manuel des balances ou le forçage de la commande (sans le contrôle de poids) afin de débloquer le tour de préparation de commande."
+        )
       ),
-      
       # Second tab content
       tabItem(tabName = "graphics",
-        h2("Some graphics")
+              h2("Some graphics")
       )
     )
   )
 )
+
