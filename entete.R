82f81b9d37aba8f58a155a290ea9b88b36bdc733
nathandjianmartin <ndmroot-git@yahoo.fr>
2022-01-17 18:11:12 +0100

    Nettoyage code pour intégration à au front Shiny

--- entete.R
+++ entete.R
@@ -70,3 +70,88 @@ balances <- merge(x = balances, y = balances_forcees, by = "BalanceId")
 
 ## = = = = = = = = Création colonne RatioForcage = = = = = = ##
 balances$RatioForcage = 100*balances$NbForcages/balances$NbUtilisations
+
+
+
+## = = = = = ACPs = = = = = ##
+
+# ACP nbUtilisations, nbTarages et NbForçages (piste qu'on a abandonné)
+acp_nb <- PCA(balances[c(2, 5, 7)])
+
+# ACP nbUtilisations et ratioErreur (piste qui nous a servi)
+acp_ratios <- PCA(balances[c(2, 4)])
+
+## = = = = = K-means = = = = ##
+
+balancesKm <- balances[c(2, 4)]
+
+set.seed(123) # ??
+resKm <- kmeans(scale(balancesKm), 4, nstart=25)
+
+
+# plot du clustering des k-means
+balancesKm$Cluster = resKm$cluster
+
+ggplot(balancesKm) +
+  geom_point(aes(x = balancesKm$NbUtilisations, y = balancesKm$RatioErreur,
+                 colour = as.factor(balancesKm$Cluster)))
+
+### = = = = Plot pour une balance = = = = ###
+# On ne garde que notre balance (TODO Modifier numBalance pour le connecter au front)
+numBalance <- "66"
+balance <- balea_data %>% filter(balea_data$BalanceId == numBalance)
+balance <- balance[c("BalanceId", "PickingDate", "TARException", "FlagWeightOverriden", "WeightRatioErreur")]
+balance <- balance[order(balance$PickingDate),]
+balance$TARSum = cumsum(balance$TARException)
+balance$FORCSum = cumsum(balance$FlagWeightOverriden)
+balance$NumUtilisation = seq.int(nrow(balance)) 
+balance$ErrorSup5 <- with(balance, ifelse(WeightRatioErreur >= 5, 1, 0))
+balance$ERRORSum = cumsum(balance$ErrorSup5)
+
+plot_tarage_balance <- ggplot(balance) +
+  geom_line(aes(x = NumUtilisation, y = TARSum)) + 
+  ylab(paste("Somme du nombre de tarages balance ", numBalance)) + 
+  xlab("Nombre d'utilisation")
+plot_forcage_balance <- ggplot(balance) +
+  geom_line(aes(x = NumUtilisation, y = FORCSum)) + 
+  ylab(paste("Somme du nombre de forçages balance ", numBalance)) + 
+  xlab("Nombre d'utilisation")
+plot_erreur_5 <- ggplot(balance) +
+  geom_line(aes(x = NumUtilisation, y = ERRORSum)) + 
+  ylab(paste("Somme du nombre d'erreur supérieur à 5% balance ", numBalance)) + 
+  xlab("Nombre d'utilisation")
+
+plot_tarage_balance
+plot_forcage_balance
+plot_erreur_5
+
+# plots du shiny
+acp_nb_aff_ind <- fviz_pca_ind(acp_nb,
+                               col.ind = "cos2", 
+                               gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+                               repel = TRUE     
+)
+acp_nb_aff_var <- fviz_pca_var(acp_nb,
+                               col.var = "contrib", 
+                               gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+                               repel = TRUE     
+)
+
+#acp_ratios <- PCA(balances[,5:6])
+acp_ratios_aff_ind <- fviz_pca_ind(acp_ratios,
+                                   col.ind = "cos2", 
+                                   gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+                                   repel = TRUE     
+)
+acp_ratios_aff_var <- fviz_pca_var(acp_ratios,
+                                   col.var = "contrib", 
+                                   gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+                                   repel = TRUE     
+)
+
+
+
+
+
+
+
